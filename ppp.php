3c58bdea1c29e8e9ecf16a1373cfe0c940a8cff3
aliazeeshan <alia.ibr20@gmail.com>
2021-10-16 10:31:23 +0100

    i love you

--- ppp.php
+++ ppp.php
@@ -17,6 +17,7 @@
         .error {
             color: red;
         }
+
     </style>
 </head>
 <body>
@@ -122,7 +123,7 @@ function test_input($data)
                 </ul>
             </div>
             <div class="card-body">
-                Developed By: AliaZeeshan
+                Developed By: Alia
             </div>
         </div>
     </div>
@@ -156,9 +157,6 @@ function test_input($data)
     </div>
     <div class="col-lg-2"></div>
 </div>
-
-<h1>ihihihi i</h1>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
