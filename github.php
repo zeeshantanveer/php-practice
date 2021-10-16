7cbafaf24145d21ed10eaf21c115cd914acb104c
Zeeshan Tanveer <z.tanveer@alignpx.com>
2021-10-16 08:48:50 +0500

    Github page completed

---
+++ github.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" class="height-full">
+<head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="assets/css/github1.css"/>
+
+    <link rel="stylesheet" href="assets/css/github2.css"/>
+
+    <link rel="stylesheet" href="assets/css/github3.css"/>
+</head>
+
+<body class="logged-out env-production page-responsive height-full d-flex flex-column header-overlay" style="word-wrap: break-word;">
+    <main class="js-warp-hide bg-gray-dark-mktg d-flex flex-auto flex-column overflow-hidden position-relative">
+        <div>
+            <div class="signup-bg-stars"></div>
+            <div class="signup-bg-stars-2"></div>
+            <div class="signup-bg-stars-3"></div>
+        </div>
+    </main>
+</body>
+</html>
+
