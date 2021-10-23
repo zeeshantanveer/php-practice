50014d3fabe0ca2727b163839ac0e06222910a99
aliazeeshan <alia.ibr20@gmail.com>
2021-10-23 09:11:08 +0100

    website

--- github.php
+++ github.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" class="height-full">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta charset="utf-8">
 
     <link rel="stylesheet" href="assets/css/github1.css"/>
@@ -10,39 +12,155 @@
     <link rel="stylesheet" href="assets/css/github3.css"/>
 
     <style>
-        #dataArray{
+        #head {
+            color: white;
+            font-family: Arial;
+            text-align: center;
+            background-image: linear-gradient(lightskyblue, lightcoral);
+            margin-top: 0px;
+        }
+        #ss{
+            text-align: center;
+            color: #00bb00;
+            margin-top: 15px;
+
+
+        }
+        #paragraph{
+            text-align: center;
+            color: #3bf0e4;
+            padding-top: 20px;
+            padding-left: 20px;
+            padding-right: 20px;
+            padding-bottom: 20px;
+            background-color: darkblue;
+            align-items: center;
+        }
+        #cd{
+            text-align: center;
+            color: #9e7bff;
+            background-color: #3300aa;
+            align-items: center;
+            text-align: center;
+
+        }
+        body {
+            margin: 0;
+            font-family: "Lato", sans-serif;
+        }
+
+        .CARD{
+            background-color: #3bf0e4;
+            margin-top: 5px;
+            margin-right: 450px;
+            text-align: center;
+        }
+        .btn-primary{
+            background-color: blue;
             color: white;
         }
+        .btn-secondary{
+            background-color: darkred;
+            color: white;
+        }
+        .btn-success{
+            background-color: orangered;
+            color: white;
+        }
+        .btn-danger{
+            background-color: #032f62;
+            color: white;
+        }
+        .btn-warning{
+            background-color: #116644;
+            color: white;
+        }
+        .btn-light{
+            background-color: #3300aa;
+            color: white;
+
+        }
+        .btn-dark{
+            background-color: deeppink;
+            color: white;
+        }
+        .btn-info{
+            background-color: purple;
+            color: white;
+        }
+        #H2{
+            background-color: lightseagreen;
+            color: white;
+            text-align: center;
+        }
+        .card-boy{
+        align-items: center;
+            background-color: #614eda;
+        }
+
     </style>
 
 </head>
-<body class="logged-out env-production page-responsive height-full d-flex flex-column header-overlay" style="word-wrap: break-word;">
+<body class="logged-out env-production page-responsive height-full d-flex flex-column header-overlay"
+      style="word-wrap: break-word;">
 <main class="js-warp-hide bg-gray-dark-mktg d-flex flex-auto flex-column overflow-hidden position-relative">
-        <div>
-            <div class="signup-bg-stars"></div>
-            <div class="signup-bg-stars-2"></div>
-            <div class="signup-bg-stars-3"></div>
-        </div>
-    <div id="dataArray">
-        <?php
-        $data['name'] = 'AliaZeeshan';
-        $data['email'] = 'AliaZeeshan@gmail.com';
-        $data['gpa'] = '3.25';
-        $data['phone'] = '1234567890';
-        echo '<pre>';
-        print_r($data);
-        echo '</pre>';
 
-        $jsonString = json_encode($data);
-        echo $jsonString;
 
-        $decodedJson = json_decode($jsonString,true);
-        echo '<pre>';
-        print_r($decodedJson);
-        echo '</pre>';
-        ?>
+    <div>
+        <div class="signup-bg-stars"></div>
+        <div class="signup-bg-stars-2"></div>
+        <div class="signup-bg-stars-3"></div>
     </div>
-    </main>
+
+    <div id="head">
+        <h1>my website</h1>
+
+
+    </div>
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+
+
+        <div class="card">
+            <div id="cd">
+                <div class="card Header"><h1>MY SELF</h1></div>
+            </div>
+            <div class="card-boy">
+                <div id="ss">
+                    <h2 class="card title"> my special page</h2>
+                </div>
+               <div id="paragraph">
+                   <p class="card-text">my name is Alia.... and i am the BS student my name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS studentmy name is Alia.... and i am the BS student</p>
+               </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"></div>
+
+
+    <div class="CARD">
+        <div id="H2">
+            <h2>my fvrt color</h2>
+        </div>
+        <div class="card-body">
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn btn-success">Success</button><br><br>
+            <button type="button" class="btn btn-danger">Danger</button>
+            <button type="button" class="btn btn-warning">Warning</button><br><br>
+            <button type="button" class="btn btn-info">Info</button>
+            <button type="button" class="btn btn-light">Light</button>
+            <button type="button" class="btn btn-dark">Dark</button>
+        </div>
+    </div>
+
+
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+
 </body>
 </html>
-
