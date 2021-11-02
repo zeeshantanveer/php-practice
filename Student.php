651b6684315115552a9fcf4f8618d9f146113505
aliazeeshan <alia.ibr20@gmail.com>
2021-11-02 07:55:42 +0000

    DB form

--- Student.php
+++ Student.php
@@ -92,9 +92,9 @@ if (!$conn) {
 // sql to create table
 $sql = "CREATE TABLE student form (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
-email VARCHAR(30) NOT NULL,
-password VARCHAR(50) NOT NULL,
+name VARCHAR(30),
+email VARCHAR (50),
+password VARCHAR(50)
 address VARCHAR (50) NOT NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
